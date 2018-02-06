c0ed73b57f1507143faa678d30c90320d5c56cc7
Charlie Blackstock <charlieblackstock@gmail.com>
2018-02-06 13:33:22 -0700

    add dynamic routing for showing campaign details

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+import { Link } from '../routes'
 
 export default class Header extends Component {
   render() {
     return (
       <Menu style={{ marginTop: '16px' }}>
-        <Menu.Item header>Crowd Coin</Menu.Item>
+        <Link route="/">
+          <a className="item">Crowd Coin</a>
+        </Link>
 
         <Menu.Menu position="right">
-          <Menu.Item>Campaigns</Menu.Item>
-          <Menu.Item>+</Menu.Item>
+          <Link route="/">
+            <a className="item">Campaigns</a>
+          </Link>
+          <Link route="/campaigns/new">
+            <a className="item">+</a>
+          </Link>
         </Menu.Menu>
       </Menu>
     )
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button, Card } from 'semantic-ui-react'
 import factory from '../ethereum/factory'
 import Layout from '../components/Layout'
+import { Link } from '../routes'
 
 export default class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -13,7 +14,11 @@ export default class CampaignIndex extends Component {
     const items = this.props.campaigns.map(address => {
       return {
         header: address,
-        description: <a>View Campaign</a>,
+        description: (
+          <Link route={`/campaigns/${address}`}>
+            <a>View Campaign</a>
+          </Link>
+        ),
         fluid: true
       }
     })
@@ -25,7 +30,16 @@ export default class CampaignIndex extends Component {
     return (
       <Layout>
         <h3>Open Campaigns</h3>
-        <Button floated="right" content="Create Campaign" icon="add" primary />
+        <Link route="/campaigns/new">
+          <a>
+            <Button
+              floated="right"
+              content="Create Campaign"
+              icon="add"
+              primary
+            />
+          </a>
+        </Link>
         {this.renderCampaigns()}
       </Layout>
     )
